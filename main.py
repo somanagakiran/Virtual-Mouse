ace7554d16a6c244073fdbd12b29d735a3887174
somanagakiran <kiranprattipati2006@gmail.com>
2025-12-01 09:55:56 +0530

    Refactor virtual mouse application with new libraries
    Refactor virtual mouse implementation to use HandTracking and autopy libraries. Simplify gesture recognition and mouse control logic.

--- main.py
+++ main.py
@@ -1,81 +1,58 @@
-"""Virtual Mouse - Main Application
-This module implements a virtual mouse system using hand gesture recognition.
-"""
-
 import cv2
 import numpy as np
-import pyautogui
-from hand_detection import HandDetector
-from gesture_recognition import GestureRecognizer
-from mouse_control import MouseController
 import time
+import HandTracking as ht
+import autopy
 
-class VirtualMouse:
-    def __init__(self):
-        self.hand_detector = HandDetector()
-        self.gesture_recognizer = GestureRecognizer()
-        self.mouse_controller = MouseController()
-        self.camera = cv2.VideoCapture(0)
-        self.fps = 30
-        self.frame_width = 1280
-        self.frame_height = 720
-        
-    def run(self):
-        """Main application loop"""
-        while True:
-            ret, frame = self.camera.read()
-            if not ret:
-                break
-                
-            # Flip the frame for selfie view
-            frame = cv2.flip(frame, 1)
-            
-            # Detect hands in the frame
-            hands = self.hand_detector.detect(frame)
-            
-            if hands:
-                hand = hands[0]
-                # Recognize gesture from hand landmarks
-                gesture = self.gesture_recognizer.recognize(hand)
-                
-                # Get hand position for mouse movement
-                x, y = self.hand_detector.get_position(hand)
-                
-                # Control mouse based on gesture
-                self.handle_gesture(gesture, x, y, frame)
-            
-            # Display frame
-            cv2.imshow('Virtual Mouse', frame)
-            
-            # Exit on 'q' key
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-                
-        self.cleanup()
-    
-    def handle_gesture(self, gesture, x, y, frame):
-        """Handle mouse control based on recognized gesture"""
-        screen_x = int(x * 1920 / self.frame_width)
-        screen_y = int(y * 1080 / self.frame_height)
-        
-        if gesture == 'move':
-            self.mouse_controller.move(screen_x, screen_y)
-        elif gesture == 'click':
-            self.mouse_controller.click(screen_x, screen_y)
-        elif gesture == 'right_click':
-            self.mouse_controller.right_click(screen_x, screen_y)
-        elif gesture == 'drag':
-            self.mouse_controller.drag(screen_x, screen_y)
-        elif gesture == 'scroll_up':
-            self.mouse_controller.scroll_up()
-        elif gesture == 'scroll_down':
-            self.mouse_controller.scroll_down()
-    
-    def cleanup(self):
-        """Clean up resources"""
-        self.camera.release()
-        cv2.destroyAllWindows()
+# Install using pip install autopy
 
-if __name__ == '__main__':
-    app = VirtualMouse()
-    app.run()
+# TITLE Variables Declaration
+pTime = 0  # Used to calculate frame rate
+width = 640  # Width of Camera
+height = 480  # Height of Camera
+frameR = 100  # Frame Rate
+smoothening = 8  # Smoothening Factor
+prevx, prevy = 0, 0  # Previous coordinates
+currx, curry = 0, 0  # Current coordinates
+
+cap = cv2.VideoCapture(0)  # Getting video feed from the webcam
+cap.set(3, width)  # Adjusting size
+cap.set(4, height)
+
+detector = ht.handDetector(maxHands=1)  # Detecting one hand at max
+screenwidth, screenheight = autopy.screen.size()  # Getting the screen size
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)  # Finding the hand
+    lmlist, bbox = detector.findPosition(img)  # Getting position of hand
+    
+    if len(lmlist) != 0:
+        x1, y1 = lmlist[8]  # Index finger tip
+        x2, y2 = lmlist[12]  # Middle finger tip
+        fingers = detector.fingersUp()  # Checking if fingers are upwards
+        
+        cv2.rectangle(img, (frameR, frameR), (width - frameR, height - frameR), (255, 0, 255), 2)  # Creating boundary box
+        
+        if fingers[1] == 1 and fingers[2] == 0:  # If fore finger is up and middle finger is down
+            x3 = np.interp(x1, (frameR, width - frameR), (0, screenwidth))
+            y3 = np.interp(y1, (frameR, height - frameR), (0, screenheight))
+            currx = prevx + (x3 - prevx) / smoothening
+            curry = prevy + (y3 - prevy) / smoothening
+            autopy.mouse.move(screenwidth - currx, curry)  # Moving the cursor
+            cv2.circle(img, (x1, y1), 7, (255, 0, 255), cv2.FILLED)
+            prevx, prevy = currx, curry
+        
+        if fingers[1] == 1 and fingers[2] == 1:  # If fore finger middle finger both are up
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+            
+            if length < 40:  # If both fingers are really close to each other
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()  # Perform Click
+    
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
